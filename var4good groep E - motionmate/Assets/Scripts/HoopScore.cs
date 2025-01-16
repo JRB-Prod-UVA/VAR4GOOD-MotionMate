ddbdfa807ca9ec3f3f6230ad3acfc6ef24a54202
lucasvg1502 <135691933+lucasvg1502@users.noreply.github.com>
2025-01-16 10:32:39 +0100

    Basketbal

---
+++ var4good groep E - motionmate/Assets/Scripts/HoopScore.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HoopScore : MonoBehaviour
+{
+    public int score = 0; // Houdt de score bij
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Controleer of het object dat de trigger raakt de bal is
+        if (other.CompareTag("Ball"))
+        {
+            score++;
+            Debug.Log("Score: " + score);
+        }
+    }
+}
